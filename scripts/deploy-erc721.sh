e189de15c77f201ae933af220da1187209e69860
Andrew Warner <warneracw@gmail.com>
2023-06-15 11:57:15 -0400

    localhost deployment

--- scripts/deploy-erc721.sh
+++ scripts/deploy-erc721.sh
@@ -1,9 +1,13 @@
 node ./scripts/top-erc721.js > configs/top-erc721.json
+
 npx graph-compiler --config configs/top-erc721.json --include src/datasources --export-schema --export-subgraph
+
 npx graph-cli build generated/top-erc721.subgraph.yaml
-graph create --node http://107.178.212.219:8020/ skillet/erc721-mainnet
+
+graph create --node http://127.0.0.1:8020/ skillet/erc721-mainnet
+
 npx graph-cli deploy                      \
-  --ipfs http://107.178.212.219:5001      \
-  --node http://107.178.212.219:8020/     \
+  --ipfs http://127.0.0.1:5001      \
+  --node http://127.0.0.1:8020/     \
   skillet/erc721-mainnet                  \
   generated/top-erc721.subgraph.yaml
